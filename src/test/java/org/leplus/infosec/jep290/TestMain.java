8eda5bf043c14b386e89237e88d9cf558c30ec7a
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 18:13:34 -0700

    Test

---
+++ src/test/java/org/leplus/infosec/jep290/TestMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.leplus.infosec.jep290;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author thomas
+ *
+ */
+class TestMain {
+
+    @Test
+    void test() throws ClassNotFoundException, IOException {
+        Main.main(null);
+    }
+
+}
